89d678f4d2b692c1efe04ca826aff65693414687
廣川　遥 <>
2020-08-06 10:56:29 +0900

    ページネーション  CSSでのスタイリング 分からず

--- archive.php
+++ archive.php
@@ -23,25 +23,15 @@
             </div>
 
             
-            
-            <?php if(function_exists("wp_pagenavi")): ?>
-            <?php wp_pagenavi(); ?>
-            <?php else: ?>
-            <?php endif; ?>
+            <div class="p-pagination--tablet">
+                <?php if(function_exists("wp_pagenavi")): ?>
+                <?php wp_pagenavi(); ?>
+                <?php else: ?>
+                <?php endif; ?>
+            </div>
             
 
-            <!-- <div class='wp-pagenavi' role='navigation'>
-                <span class='pages'>1 / 6</span>
-                <span aria-current='page' class='current'>1</span>
-                <a class="page larger" title="Page 2" href="http://hamburgerwp.local/category/takeout/page/2/">2</a>
-                <a class="page larger" title="Page 3" href="http://hamburgerwp.local/category/takeout/page/3/">3</a>
-                <a class="page larger" title="Page 4" href="http://hamburgerwp.local/category/takeout/page/4/">4</a>
-                <a class="page larger" title="Page 5" href="http://hamburgerwp.local/category/takeout/page/5/">5</a>
-                <span class='extend'>...</span>
-                <a class="nextpostslink" rel="next" href="http://hamburgerwp.local/category/takeout/page/2/">»</a>
-                <a class="last" href="http://hamburgerwp.local/category/takeout/page/6/">最後 »</a>
-            </div>                         -->
-
+            
 
             <!-- <div class="p-pagination--tablet">
                 <p>page 1/10</p>
