82a4012a87338e07291e457936753a0ff381e874
廣川　遥 <>
2020-08-06 20:38:06 +0900

    スマホ画面　ページング　実装

--- archive.php
+++ archive.php
@@ -18,8 +18,8 @@
             
 
             <div class="p-pagination--sp">
-                <button>＜＜ 前へ</button>
-                <button>次へ ＞＞</button>
+                <button><?php next_posts_link('＜＜ 前へ'); ?></button>
+                <button><?php previous_posts_link('次へ ＞＞'); ?></button>
             </div>
 
             
@@ -30,23 +30,6 @@
                 <?php endif; ?>
             </div>
             
-
-            
-
-            <!-- <div class="p-pagination--tablet">
-                <p>page 1/10</p>
-                <button class="p-pagination--tablet__pointer">＜＜</button>
-                <button class="p-pagination--tablet__button">1</button>
-                <button class="p-pagination--tablet__button">2</button>
-                <button class="p-pagination--tablet__button">3</button>
-                <button class="p-pagination--tablet__button">4</button>
-                <button class="p-pagination--tablet__button">5</button>
-                <button class="p-pagination--tablet__button">6</button>
-                <button class="p-pagination--tablet__button">7</button>
-                <button class="p-pagination--tablet__button">8</button>
-                <button class="p-pagination--tablet__button">9</button>
-                <button class="p-pagination--tablet__pointer">＞＞</button> 
-            </div> -->
             
         </div><!-- .l-main -->
 
