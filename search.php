93b5f21c51b8d9f15297969ecf470ccf4c4d1653
廣川　遥 <>
2020-08-07 11:15:51 +0900

    新規作成

--- search.php
+++ search.php
@@ -3,51 +3,30 @@
             
             <div class="p-topimage02">
                 <img src="<?php bloginfo('template_url'); ?>/images/PCarchive_title.jpg" alt="ハンバーガーとオニオンリング">
-                <?php if(is_category()) : ?>
-                <h1 class="p-topimage02__title">Menu:<br><span><?php single_cat_title(); ?></span></h1>
-                <? endif; ?>
+                <h1 class="p-topimage02__title">Search:<br><span><?php the_search_query(); ?></span></h1><!-- 検索結果ページに検索文字列を表示 -->
             </div>
             
 
             <div class="p-lead">
-                <h2>小見出しが入ります</h2>
-                <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+                <?php echo category_description(); ?>
             </div>
 
 
+            <?php get_template_part("components/archive-article"); ?>
             
-            <?php if (have_posts()): ?>
-            <?php
-            if (isset($_GET['s']) && empty($_GET['s'])) {
-                echo '検索キーワード未入力'; // 検索キーワードが未入力の場合のテキストを指定
-            } else {
-                echo '“'.$_GET['s'] .'”の検索結果：'.$wp_query->found_posts .'件'; // 検索キーワードと該当件数を表示
-            }
-            ?>
-            <ul>
-            <?php while(have_posts()): the_post(); ?>
-            <li>
-            <a href="<?php the_permalink(); ?>"><?php echo get_the_title(); ?></a>
-            </li>
-            <?php endwhile; ?>
-            </ul>
-            <?php else: ?>
-            検索されたキーワードにマッチする記事はありませんでした
-            <?php endif; ?>
-             
-
 
             <div class="p-pagination--sp">
-                <button>＜＜ 前へ</button>
-                <button>次へ ＞＞</button>
+                <button><?php next_posts_link('＜＜ 前へ'); ?></button>
+                <button><?php previous_posts_link('次へ ＞＞'); ?></button>
             </div>
 
             
-            
-            <?php if(function_exists("wp_pagenavi")): ?>
-            <?php wp_pagenavi(); ?>
-            <?php else: ?>
-            <?php endif; ?>
+            <div class="p-pagination--tablet">
+                <?php if(function_exists("wp_pagenavi")): ?>
+                <?php wp_pagenavi(); ?>
+                <?php else: ?>
+                <?php endif; ?>
+            </div>
 
             
         </div><!-- .l-main -->
