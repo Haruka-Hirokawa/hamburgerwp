481b0f902f708a7adcd73437c567a16f3b0e7a72
廣川　遥 <>
2020-08-07 19:04:44 +0900

    固定ページへのリンク記入

--- footer.php
+++ footer.php
@@ -1,8 +1,8 @@
     <footer class="l-footer">
         <ul>
-            <li><a href="#">ショップ情報</a></li>
+            <li><a href="<?php echo get_page_link( '17' ); ?>">ショップ情報</a></li>
             <li>|</li>
-            <li><a href="#">ヒストリー</a></li>
+            <li><a href="<?php echo get_page_link( '79' ); ?>">ヒストリー</a></li>
         </ul>
         <div class="l-footer__copyright">
             <p><small>&copy; Raise Tech</small></p>
