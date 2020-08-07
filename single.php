3239145d564811c509f5d010c21a70ada6142dae
廣川　遥 <>
2020-08-07 15:49:28 +0900

    ループ処理 挿入

--- single.php
+++ single.php
@@ -1,151 +1,22 @@
 <?php get_header(); ?>
 
+            <?php if(have_posts()): while(have_posts()): the_post(); ?>
+
             <div class="p-topimage01">
-                <img src="images/SPsingle_title.jpg" class="p-topimage01--sp" alt="スマホ用タイトル画像">
-                <img src="images/PCsingle_title.jpg" class="p-topimage01--pc" alt="パソコン用タイトル画像">
-                <h1 class="p-topimage01__title">h1 チーズバーガー</h1>
+            <?php if(has_post_thumbnail()) { ?>
+                        <?php the_post_thumbnail(); ?> 
+                    <?php } else { ?>
+                         <img src="<?php bloginfo('template_url'); ?>/images/noimage.png" alt="no-image" class="c-card__img">
+                    <?php } ?>
+                <h1 class="p-topimage01__title"><?php the_title(); ?></h1>
             </div>
 
-            <div class="p-heading">
-                <h2>見出しh2</h2>
-                <p>pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。</p>
-
-                <h3>見出しh3</h3>
-                <h4>見出しh4</h4>
-                <h4>見出しh3</h4>
-                <h4>見出しh4</h4>
-                <h4>見出しh5</h4>
-                <h4>見出しh6</h4>
-            </div>
-
-            <div class="c-quote">
-                <p>Blockquote 引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ</p>
-
-                <p>出典元：<a href="#">◯◯◯◯◯◯◯◯◯◯</a></p>
-            </div>
-
-            <div class="c-img--large">
-                <img src="images/PCsingle_contents.jpg" alt="ハンバーガーとフライドポテト">
-            </div>
+            <?php the_content(); ?>
             
-
-            <div class="c-gridContents">
-                <div class="c-gridContents__picture">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                <div class="c-gridContents__text">
-                    <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                </div>
-                
-            </div>
-
-            <div class="c-gridContents">
-                <div class="c-gridContents__text">
-                    <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                </div>
-                <div class="c-gridContents__picture">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                
-            </div>
-
-            <div class="c-img--small">
-                <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-            </div>
-
-            <div class="c-gridImages">
-                <div class="c-gridImages__image">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                <div class="c-gridImages__image">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                <div class="c-gridImages__image">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                <div class="c-gridImages__image">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                <div class="c-gridImages__image">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                <div class="c-gridImages__image">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                <div class="c-gridImages__image">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                <div class="c-gridImages__image">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                <div class="c-gridImages__image">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-            </div>
-
-            <div class="c-list">
-                <ol>
-                    <li>リストリストリスト</li>
-                    <li>リストリストリスト
-                        <ol>
-                            <li>リスト2リスト2リスト2</li>
-                            <li>リスト2リスト2リスト2</li>
-                        </ol>
-                    </li>
-                    <li value="1">リストリストリスト</li> <!-- リストの番号を途中で変更 -->
-                    <li>リストリストリスト</li>
-                </ol>
-            </div>
-
-            <div class="c-list">
-                <ul>
-                    <li>リストリストリスト</li>
-                    <li>リストリストリスト
-                        <ul>
-                            <li>リスト2リスト2リスト2</li>
-                            <li>リスト2リスト2リスト2</li>
-                        </ul>
-                    </li>
-                    <li>リストリストリスト</li>
-                    <li>リストリストリスト</li>
-                </ul>
-            </div>
-
-            <div class="c-tag">
-                <!-- ↓タグを表示するための特殊文字。< は &lt; 、> は &gt; -->
-                <p>&lt;html&gt;</p>
-                <p>　&lt;head&gt;</p>
-                <p>　&lt;head&gt;</p>
-                <p>　&lt;body&gt;</p>
-                <p>　&lt;body&gt;</p>
-                <p>&lt;/html&gt;</p>
-            </div>
-
-            <table class="c-table" border="1" style="border-collapse: collapse"><!-- border属性 1→二重線になる、style属性を使って二重線を一つの線に変える -->            
-                <tr>
-                    <td>テーブル</td>
-                    <td>テーブル</td>
-                </tr>
-                <tr>
-                    <td>テーブル</td>
-                    <td>テーブル</td>
-                </tr>
-                <tr>
-                    <td>テーブル</td>
-                    <td>テーブル</td>
-                </tr>
-                <tr>
-                    <td>テーブル</td>
-                    <td>テーブル</td>
-                </tr>
-            </table>
-
-            <input type="submit" value="ボタン" name="button" id="button" class="c-button">
-            <label for="button" class="c-button-label">ボタン</label>
-
-            <p class="bold">boldboldboldboldboldbold</p>
+            <?php endwhile; else: ?>
+            <?php endif; ?>
             
-        </div> <!-- .l-main -->
+        </div><!-- .l-main -->
 
         <!-- サイドバー -->
         <?php get_sidebar(); ?>
@@ -155,3 +26,6 @@
     
     
     <?php get_footer(); ?>
+
+
+    <!-- <img src="http://hamburgerwp.local/wp-content/uploads/2020/07/side_potate.jpg" class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="フライドポテト" srcset="http://hamburgerwp.local/wp-content/uploads/2020/07/side_potate.jpg 640w, http://hamburgerwp.local/wp-content/uploads/2020/07/side_potate-300x200.jpg 300w" sizes="(max-width: 640px) 100vw, 640px"> -->
