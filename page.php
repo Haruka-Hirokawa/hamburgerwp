548aa7b2232cdef30078116e5ce133a77381a47f
廣川　遥 <>
2020-08-14 12:17:33 +0900

    page.php 編集　固定ページ表示

--- page.php
+++ page.php
@@ -1,149 +1,19 @@
 <?php get_header(); ?>
 
-            <div class="p-topimage01">
-                <img src="images/SPsingle_title.jpg" class="p-topimage01--sp" alt="スマホ用タイトル画像">
-                <img src="images/PCsingle_title.jpg" class="p-topimage01--pc" alt="パソコン用タイトル画像">
-                <h1 class="p-topimage01__title">h1 チーズバーガー</h1>
+            <div class="p-topimage03">
+                <img src="<?php bloginfo('template_url'); ?>/images/PCpage_title.jpg" alt="ショップ">
+                <h1 class="p-topimage01__title"><?php the_title(); ?></h1>
             </div>
 
-            <div class="p-heading">
-                <h2>見出しh2</h2>
-                <p>pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。pタグテキスト。</p>
+            <?php if(have_posts()): while(have_posts()): the_post(); ?>
 
-                <h3>見出しh3</h3>
-                <h4>見出しh4</h4>
-                <h4>見出しh3</h4>
-                <h4>見出しh4</h4>
-                <h4>見出しh5</h4>
-                <h4>見出しh6</h4>
+            <div class="p-editor-style">
+                <?php the_content(); ?>
             </div>
 
-            <div class="c-quote">
-                <p>Blockquote 引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ引用タグ</p>
-
-                <p>出典元：<a href="#">◯◯◯◯◯◯◯◯◯◯</a></p>
-            </div>
-
-            <div class="c-img--large">
-                <img src="images/PCsingle_contents.jpg" alt="ハンバーガーとフライドポテト">
-            </div>
-            
-
-            <div class="c-gridContents">
-                <div class="c-gridContents__picture">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                <div class="c-gridContents__text">
-                    <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                </div>
-                
-            </div>
-
-            <div class="c-gridContents">
-                <div class="c-gridContents__text">
-                    <p>テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-                </div>
-                <div class="c-gridContents__picture">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                
-            </div>
-
-            <div class="c-img--small">
-                <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-            </div>
-
-            <div class="c-gridImages">
-                <div class="c-gridImages__image">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                <div class="c-gridImages__image">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                <div class="c-gridImages__image">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                <div class="c-gridImages__image">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                <div class="c-gridImages__image">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                <div class="c-gridImages__image">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                <div class="c-gridImages__image">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                <div class="c-gridImages__image">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-                <div class="c-gridImages__image">
-                    <img src="images/PCsingle_gridcontents.jpg" alt="ハンバーガーとフライドポテト">
-                </div>
-            </div>
-
-            <div class="c-list">
-                <ol>
-                    <li>リストリストリスト</li>
-                    <li>リストリストリスト
-                        <ol>
-                            <li>リスト2リスト2リスト2</li>
-                            <li>リスト2リスト2リスト2</li>
-                        </ol>
-                    </li>
-                    <li value="1">リストリストリスト</li> <!-- リストの番号を途中で変更 -->
-                    <li>リストリストリスト</li>
-                </ol>
-            </div>
-
-            <div class="c-list">
-                <ul>
-                    <li>リストリストリスト</li>
-                    <li>リストリストリスト
-                        <ul>
-                            <li>リスト2リスト2リスト2</li>
-                            <li>リスト2リスト2リスト2</li>
-                        </ul>
-                    </li>
-                    <li>リストリストリスト</li>
-                    <li>リストリストリスト</li>
-                </ul>
-            </div>
-
-            <div class="c-tag">
-                <!-- ↓タグを表示するための特殊文字。< は &lt; 、> は &gt; -->
-                <p>&lt;html&gt;</p>
-                <p>　&lt;head&gt;</p>
-                <p>　&lt;head&gt;</p>
-                <p>　&lt;body&gt;</p>
-                <p>　&lt;body&gt;</p>
-                <p>&lt;/html&gt;</p>
-            </div>
-
-            <table class="c-table" border="1" style="border-collapse: collapse"><!-- border属性 1→二重線になる、style属性を使って二重線を一つの線に変える -->            
-                <tr>
-                    <td>テーブル</td>
-                    <td>テーブル</td>
-                </tr>
-                <tr>
-                    <td>テーブル</td>
-                    <td>テーブル</td>
-                </tr>
-                <tr>
-                    <td>テーブル</td>
-                    <td>テーブル</td>
-                </tr>
-                <tr>
-                    <td>テーブル</td>
-                    <td>テーブル</td>
-                </tr>
-            </table>
-
-            <input type="submit" value="ボタン" name="button" id="button" class="c-button">
-            <label for="button" class="c-button-label">ボタン</label>
-
-            <p class="bold">boldboldboldboldboldbold</p>
+            <?php endwhile; else: ?>
+                <p>お探しのページはありません</p>
+            <?php endif; ?>
             
         </div> <!-- .l-main -->
 
