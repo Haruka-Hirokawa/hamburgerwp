e2b9bc68151bda1b7781f01890d77b3f530277c2
廣川　遥 <>
2020-08-15 13:54:25 +0900

    テーマチェック 修正追加 echo esc_url( home_url( '/' ) );

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
-                <form class="l-header__searchform" name="searchform" id="searchform" method="get" action="<?php echo home_url('/'); ?>">
+                <form class="l-header__searchform" name="searchform" id="searchform" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
                     <input class="l-header__searchform__keywords" type="search" value="" name="s" id="s" >
-                    <label for="keywords" class="screen-reader-text">検索ワード<!-- ←labelの内容 省略不可 --></label>
+                    <label for="keywords" class="screen-reader-text"><?php echo __(' search', 'hamburgerwp' ); ?>検索ワード<!-- ←labelの内容 省略不可 --></label>
                     <!-- ↑label for…文字列“検索ワード”が何なのかをブラウザに教える -->
                     
                     <input class="l-header__searchform__searchbutton" type="submit" value="検索" name="searchbutton" id="searchbutton">
